a25b15b9df1662ffe5ff293bea53a3ab0072fe59
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-26 10:10:29 +0900

    add on keyword

--- lib/ruboty/handlers/qanda.rb
+++ lib/ruboty/handlers/qanda.rb
@@ -3,6 +3,10 @@ require "ruboty/qanda/actions/qanda"
 module Ruboty
   module Handlers
     class Qanda < Base
+      on /tell( me)? (?<keyword>.+)/, name: "qanda", description: "Answer your question"
+      def qanda(message)
+        Ruboty::Qanda::Actions::Qanda.new(message).call
+      end
     end
   end
 end
