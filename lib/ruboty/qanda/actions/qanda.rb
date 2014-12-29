a1072b152809d5fa8d972dff1b27b1d5f9570357
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-29 15:34:10 +0900

    add Faraday connection

--- lib/ruboty/qanda/actions/qanda.rb
+++ lib/ruboty/qanda/actions/qanda.rb
@@ -2,6 +2,43 @@ module Ruboty
   module Qanda
     module Actions
       class Qanda < Ruboty::Actions::Base
+        DOCOMO_QANDA_API_URL = "https://api.apigw.smt.docomo.ne.jp/knowledgeQA/v1/ask"
+
+        def initialize(message)
+          @message = message
+        end
+
+        def call
+          message.reply(qanda)
+        end
+
+        private
+
+        def qanda
+        end
+
+        def response
+          connection.get(url, params)
+        end
+
+        def connection
+          Faraday.new do |connection|
+            connection.request  :url_encoded
+            connection.adapter  :net_http
+            connection.response :json
+          end
+        end
+
+        def url
+          DOCOMO_QANDA_API_URL
+        end
+
+        def params
+          {
+            api_key: ENV["DOCOMO_API_KEY"],
+            q: @message
+          }
+        end
       end
     end
   end
